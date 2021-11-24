8eb44f280f740ffcc595b4aba6fd33de66d01dde
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-24 13:05:41 +0300

    add the search

--- news/views.py
+++ news/views.py
@@ -1,25 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
+from .models import Article
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
 
-def news_of_day(request):
+def news_today(request):
     date = dt.date.today()
     return render(request, 'all-news/today-news.html', {"date": date,})
     
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    # day = convert_dates(date)
-    # html = f'''
-    #     <html>
-    #         <body>
-    #             <h1> News for {day} {date.day}-{date.month}-{date.year}</h1>
-    #         </body>
-    #     </html>
-    #         '''
-    # return HttpResponse(html)
 
 def convert_dates(dates):
 
@@ -44,6 +35,24 @@ def past_days_news(request, past_date):
         assert False
 
     if date == dt.date.today():
-        return redirect(news_of_day)
+        return redirect(news_today)
 
     return render(request, 'all-news/past-news.html', {"date": date})
+
+def news_today(request):
+    date = dt.date.today()
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
